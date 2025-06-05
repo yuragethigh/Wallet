b5eb77446f99d7c8f7735c16cd94ef41186612c3
yuragethigh <yourselfmind@yandex.ru>
2025-06-05 13:36:28 +0300

    Initial Commit

---
+++ Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by Yuriy on 05.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
